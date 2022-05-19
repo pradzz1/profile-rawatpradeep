6da8f23f4264aa50475196348c33cdf953947163
pradzz1 <pradzz.rawat6@gmail.com>
2022-05-19 17:51:54 +0530

    new profile

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  NavLink
+  Route
 } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home"
@@ -11,39 +10,11 @@ import aboutMe from "./components/aboutMe";
 import contact from "./components/contact";
 import experience from "./components/totalExperience";
 import footer from "./components/footer";
-
+import HeaderNav from "./components/Navigation"
 
 function App() {
-  return (
-    
-    <Router>
-      <div>
-      
-      <div id="header" className="clearfix">
-     <div className="ant-row">
-     <div className="ant-col ant-col-xs-24 ant-col-sm-24 ant-col-md-6 ant-col-lg-6 ant-col-xl-5 ant-col-xxl-4"><h1>
-       <a id="logo" href="/profile-rawatpradeep">Pradeep Rawat - Developer</a></h1>
-       </div>
-       <div className="ant-col menu-row ant-col-xs-0 ant-col-sm-0 ant-col-md-18 ant-col-lg-18 ant-col-xl-9 ant-col-xxl-20"></div>
-       <div className="ant-col ant-col-xs-24 ant-col-sm-24 ant-col-md-6 ant-col-lg-6 ant-col-xl-5 ant-col-xxl-4">
-
-      <NavLink to="/profile-rawatpradeep" exact className="link" activeClassName="active">
-          Home
-        </NavLink>
-        <NavLink to="/aboutMe" className="link" activeClassName="active">
-          About Me
-        </NavLink>
-        <NavLink to="/experience" className="link" activeClassName="active">
-          Experience
-        </NavLink>
-        <NavLink to="/contact" className="link" activeClassName="active">
-          Contact
-        </NavLink>
-     
-        </div>
-        </div>
-      </div>
-      <div>
+  return (<Router>  
+    <HeaderNav/>
       <div className="main-wrapper">
       <Switch>
         <Route path="/profile-rawatpradeep" exact children={Home} />
@@ -53,14 +24,7 @@ function App() {
          
         </Switch>
         </div>
-      </div>
-      
   <div>{footer}</div>
-  
-       
-
-       
-      </div>
     </Router>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,36 +1,43 @@
 import React from 'react';
 //import filmStripe from './filmstripe';
-import SectionHeader from './sectionHeader';
-
-
 const home = (props) =>{
     return(
-      <>
-        <div className="main-hero-text">
-          <div className="content">
-            <div className="text-loading-mask">
-              <div className="text-loading-overlay">
-                </div>
-                <SectionHeader name="Home"></SectionHeader>
-                </div>
-                <div>
-                  <div className="text-loading-mask">
-                    <div className="text-loading-overlay">
-                      </div>
-                      <h3 itemProp="specialty" className="main-hero-subtitle h5">Front-end Developer</h3>
-                      </div>
-                      </div>
-                      <div>
-                        <div  className="text-loading-mask">
-                          <div className="text-loading-overlay">
-                        </div>
-                        <h3 itemProp="specialty" className="main-hero-subtitle h5">Front-end Developer</h3>
-                  </div>
-                </div>
-              </div>
-          </div>
+      <div className="relative bg-white overflow-hidden">
+      <div className="max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <svg
+            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
 
-        </>
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">Hi I m Pradeep Rawat</span>{' '}
+                <span className="block text-indigo-600 xl:inline">Full Stack Developer</span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              I am a web developer from Dehradun, Uttarakhand and currently living in Bangalore.
+              I enjoy Building everything from small business sites to rich interactive web apps.
+              If you are a business seeking a web presence or any employer looking to hire, you can get in touch with me here.
+                         </p>
+            </div>
+          </main>
+        </div>
+      </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img
+          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          src="https://ik.imagekit.io/pradeeprawat/image4_PDjeSmxL_JzK.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1644153361018"
+          alt=""
+        />
+      </div>
+    </div>
     );
 }
 export default home;
